2a2a6024aef7362f35be7b6992f6d97b75f54425
jiyaaany <jiyaaany@gmail.com>
2020-09-21 10:27:56 +0900

    Updates

---
+++ precache-manifest.1154e55381346c21cb633a5ea04e7a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb92550c1a7ce902eb61119f8556e26",
+    "url": "/mashup-todolist/index.html"
+  },
+  {
+    "revision": "b039d0f8ad79ac1e64a8",
+    "url": "/mashup-todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a9afb8dad952712008de",
+    "url": "/mashup-todolist/static/js/2.bc2a4ce8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mashup-todolist/static/js/2.bc2a4ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b039d0f8ad79ac1e64a8",
+    "url": "/mashup-todolist/static/js/main.e8ee7372.chunk.js"
+  },
+  {
+    "revision": "c629f1422fece74ad952",
+    "url": "/mashup-todolist/static/js/runtime-main.ea093a2d.js"
+  }
+]);
